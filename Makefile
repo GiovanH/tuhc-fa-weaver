c59012c4c6c60867a23a1eb75c8c11eb893171ef
Gio <sethg@ipi.org>
2024-06-07 23:03:59 -0500

    Rubyquest postprocessing

--- Makefile
+++ Makefile
@@ -9,20 +9,20 @@ PYTHON=py -3
 default: ruby-online ruby-full nan-online nan-full
 
 define postprocess
-$(PYTHON) -c 'from rubyquest import *; encoolen("./$(1)/")'
+$(PYTHON) -c 'from rubyquest import *; fix_timestamps("./$(1)/")'
 endef
 
 .PHONY: ruby-full
 ruby-full:
 	$(PYTHON) $(TUHC_DIR)/tools/mspfa/mspfa.py $(STORY_ID)
-	# $(call postprocess,$(STORY_NAME))
+	$(call postprocess,$(STORY_NAME))
 	env MOD_TITLE="$(STORY_NAME)" \
 	  j2 mod.js.j2 --print > "$(STORY_NAME)/mod.js"
 
 .PHONY: ruby-online
 ruby-online:
 	$(PYTHON) $(TUHC_DIR)/tools/mspfa/mspfa.py $(STORY_ID) --online
-	# $(call postprocess,$(STORY_NAME)_online)
+	$(call postprocess,$(STORY_NAME)_online)
 	env MOD_TITLE="$(STORY_NAME) (online)" \
 	  j2 mod.js.j2 --print > "$(STORY_NAME)_online/mod.js"
 
@@ -32,6 +32,8 @@ ruby-online:
 nan-full:
 	$(PYTHON) $(TUHC_DIR)/tools/mspfa/mspfa.py 23588
 	# $(call postprocess,$(STORY_NAME))
+	echo -e "\n} // manual fix\n" >> "Nan Quest/adventure.css"
+	echo -e "} // manual fix\n" >> "Nan Quest/adventure.scss"
 	. ./env_nan && env MOD_TITLE="Nan Quest" \
 	  j2 mod.js.j2 --print > "Nan Quest/mod.js"
 
@@ -39,5 +41,7 @@ nan-full:
 nan-online:
 	$(PYTHON) $(TUHC_DIR)/tools/mspfa/mspfa.py 23588 --online
 	# $(call postprocess,Nan Quest_online)
+	echo -e "\n} // manual fix\n" >> "Nan Quest_online/adventure.css"
+	echo -e "} // manual fix\n" >> "Nan Quest_online/adventure.scss"
 	. ./env_nan && env MOD_TITLE="Nan Quest (online)" \
 	  j2 mod.js.j2 --print > "Nan Quest_online/mod.js"
--- rubyquest.py
+++ rubyquest.py
@@ -1,21 +1,10 @@
 #!/bin/python3
-# Run mspfa.py <story_slug> <mspfa_id>
-# e.g. python3 mspfa.py "Felt" 21
 
-# import re
 import json
 import os
-# import requests
 import bs4
-# import urllib.parse
-# import logging
-# import difflib
-# import itertools
-# from urllib.parse import urlparse
-# from lib import TriadLogger
 import shutil
 import traceback
-# import cgi
 import glob
 import typing
 
@@ -37,7 +26,7 @@ yaml.representer.add_representer(str, _str_presenter)
 try:
     from tqdm import tqdm
 except ImportError:
-    def tqdm(iterator, *args, **kwargs):
+    def tqdm(iterator, *args, **kwargs):  # type: ignore[no-redef]  # noqa: ARG001
         yield from iterator
 
 wgetroot = 'L:/Archive/Homestuck/suptg.thisisnotatrueending.com/archive/'
@@ -53,44 +42,46 @@ with open('./alllines.txt', 'w', encoding="utf-8") as fp:
 with open('./ruby.json', 'r') as fp:
     ruby_dupes = json.load(fp)
 
-known_pairs: typing.Mapping[str, str] = {}
+
+map_page_to_image: typing.Dict[str, str] = {}
 for line in all_lines:
     if '<span class="commentpostername">Weaver</span>' in line:
         soup = bs4.BeautifulSoup(line)
-        name = soup.find(class_='commentpostername')
+        name: bs4.Tag = soup.find(class_='commentpostername')  # type: ignore[assignment]
         if name:
             name.decompose()
-        trip = soup.find(class_='postertrip')
+        trip: bs4.Tag = soup.find(class_='postertrip')  # type: ignore[assignment]
         if trip:
             trip.decompose()
 
         image_links = soup.select('span.filesize a')
         if image_links:
             filename: str = os.path.split(image_links[0]['href'])[1]
+            assert isinstance(filename, str)
             # print(filename)
-            for set in ruby_dupes['matchSets']:
-                if any([
+            for dupeset in ruby_dupes['matchSets']:
+                if any(
                     file['filePath'].endswith(filename)
-                    for file in set['fileList']
-                ]):
-                    # print(set['fileList'])
+                    for file in dupeset['fileList']
+                ):
+                    # print(dupeset['fileList'])
                     assets = [
                         os.path.split(file['filePath'])[1]
-                        for file in set['fileList']
+                        for file in dupeset['fileList']
                         if file['filePath'].startswith('TUHC')
                     ]
                     if assets:
                         if len(assets) > 1:
                             print("Identical:", assets)
                         else:
-                            page_num = assets[0].split('_')[0].split('.')[0]
-                            known_pairs[page_num] = filename
+                            page_num: str = assets[0].split('_')[0].split('.')[0]
+                            map_page_to_image[page_num] = filename
                         # print(assets[0])
         else:
             continue
             # raise NotImplementedError(soup)
 
-# print(known_pairs)
+# print(map_page_to_image)
 
 all_docs: dict = {}
 def docFor(page) -> bs4.BeautifulSoup:
@@ -105,13 +96,13 @@ def docFor(page) -> bs4.BeautifulSoup:
             query = textstrs[-2]
             line = [s for s in all_lines if query.replace(',', '&#44;') in s][0]
         except IndexError:
-            print(query)
+            # print(textstrs, query)
             raise
     soup = bs4.BeautifulSoup(line)
     return soup
 
 
-def encoolen(mod_dir) -> None:
+def fix_timestamps(mod_dir) -> None:
     story_yaml_path = os.path.join(mod_dir, "story.yaml")
     story_yaml_bakpath = story_yaml_path + ".orig"
     if not os.path.isfile(story_yaml_bakpath):
@@ -120,46 +111,55 @@ def encoolen(mod_dir) -> None:
     with open(story_yaml_bakpath, 'r', encoding="utf-8") as fp:
         story_data = yaml.load(fp)
 
-    prev_match: typing.Optional[int] = None
-    for page in story_data['p']:
-        doc_time_match: typing.Optional[str] = known_pairs.get(str(page['i']))
+    prev_doc_time: typing.Optional[int] = None
+    for page in story_data['p'][1:]:
+        doc_time_match: typing.Optional[str] = map_page_to_image.get(str(page['i']))
+
         if isinstance(doc_time_match, str):
             doc_time: int = int(doc_time_match.split('.')[0])
-            if prev_match:
-                if doc_time < prev_match:
-                    raise ValueError(f"{page}: Last doc_time {doc_time} < prev {prev_match}")
-            assert (not prev_match) or doc_time >= prev_match
+            if prev_doc_time:
+                if doc_time < prev_doc_time:
+                    # raise ValueError(f"{page}: Last doc_time {doc_time} < prev {prev_doc_time}")
+                    print("[Matched] Shifting page time", doc_time, prev_doc_time, (prev_doc_time - doc_time), page)
+                    # doc_time = prev_doc_time
+            # assert (not prev_doc_time) or doc_time >= prev_doc_time
             page['d'] = doc_time
-            prev_match = doc_time
+            prev_doc_time = doc_time
+
         else:
-            page['d'] = prev_match or page['d']
-            # print("no known", page['i'], match, prev_match)
+            page['d'] = prev_doc_time or page['d']
+            # print("no known", page['i'], match, prev_doc_time)
             try:
                 soup = docFor(page)
                 image_links = soup.select('span.filesize a')
                 if image_links:
-                    filename: typing.Optional[str] = os.path.split(image_links[0]['href'])[1]
-                doc_time: int = int(filename.split('.')[0])
-                if prev_match:
-                    if doc_time < prev_match:
-                        raise ValueError(f"{page}: Last doc_time {doc_time} < prev {prev_match}")
-                assert (not prev_match) or doc_time >= prev_match
+                    filename: str = os.path.split(image_links[0]['href'])[1]
+                    doc_time: int = int(filename.split('.')[0])
+                if prev_doc_time:
+                    if doc_time < prev_doc_time:
+                        # raise ValueError(f"{page}: Last doc_time {doc_time} < prev {prev_doc_time}")
+                        print("[Unmatched] Shifting page time", doc_time, prev_doc_time, (prev_doc_time - doc_time), page)
+                        # doc_time = prev_doc_time
+                # assert (not prev_doc_time) or doc_time >= prev_doc_time
                 page['d'] = doc_time
-                prev_match = doc_time
+                prev_doc_time = doc_time
             except Exception:
-                traceback.print_exc()
+                # print(page)
+                # raise
+                # traceback.print_exc()
                 continue
         # try:
             # print(docFor(page))
         # except:
         #     continue
 
+    print("Saving...")
     with open(story_yaml_path, 'w', encoding="utf-8") as fp:
         yaml.dump(story_data, fp)
 
 
 if __name__ == "__main__":
     try:
-        encoolen("./Ruby Quest/")
+        fix_timestamps("./Ruby Quest/")
     except KeyboardInterrupt:
         os.abort()
